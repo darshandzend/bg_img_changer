3ca656e55caf683828f587745d375fa4ef62b955
Darshan Zend <darshandzend@gmail.com>
2015-09-01 18:50:19 +0530

    Added dictionary
    > Better code
    > Workaround for changing bg properties

--- bgchange.js
+++ bgchange.js
@@ -1,20 +1,23 @@
 url = document.URL
 
-board1 = "https://url_to_board1";
-board2 = "https://url_to_board2";
-//add as many as you need
 
-switch(url) {
-    case board1:
-        bg_img= 'http://www.planwallpaper.com/static/images/Seamless-Polygon-Backgrounds-Vol2-full_Kfb2t3Q.jpg'; 
-        break;
-    case board2:
-        bg_img= 'http://some_other_image.jpg'; 
-        break;
-    //add accordingly
+var urlToImg = {
+    "https://trello.com/b/BjezGZey/next-actions": "http://www.planwallpaper.com/static/images/Seamless-Polygon-Backgrounds-Vol2-full_Kfb2t3Q.jpg"
+
+    //add your mapping here
 }
 
-if(bg_img) {
-    b = document.getElementsByTagName("body")[0]
-    b.setAttribute("style", "background-image: url('" + bg_img + "')");
+bg_img = (url in urlToImg) ? urlToImg[url] : "";
+
+setTimeout(changebg, 5000) //wait till _ALL_ the initial XHRs are complete
+
+function changebg(){
+    if(bg_img != "") {
+        console.log("bg_img " + bg_img)
+        b = document.getElementsByTagName("body")[0]
+        console.log(b)
+    //    b.setAttribute("position", "foobar");
+        b.setAttribute("style", "background-image: url('" + bg_img + "')");
+        console.log(b)
+    }
 }
